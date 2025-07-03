87a928a9d49f6fe97cb258221d06f807f512c1af
louissosthenes9 <louissosthenes9@gmail.com>
2025-07-03 09:13:33 +0300

    users

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,4 +1,3 @@
-
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from users.models import StudentProfile
@@ -8,30 +7,25 @@ from universities.api.serializers import UniversitySerializer
 
 User = get_user_model()
 
-class StudentProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer(source='user', read_only=True)
-    university_details = UniversitySerializer(source='university', read_only=True)
-    
-    class Meta:
-        model = StudentProfile
-        fields = ['id', 'university', 'university_details', 'course', 'created_at', 'updated_at', 'user']
-        read_only_fields = ['id', 'created_at', 'updated_at', 'user', 'university_details']
-        extra_kwargs = {
-            'university': {'write_only': True}
-        }
-
 class UserSerializer(serializers.ModelSerializer):
-    student_profile = StudentProfileSerializer(read_only=True)
-    
+    student_profile = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
     
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'mobile','last_name', 
-                  'password', 'roles','profile_pic',
-                  'student_profile', 
+        fields = ['id', 'email', 'first_name', 'mobile', 'last_name',
+                  'password', 'roles', 'profile_pic',
+                  'student_profile',
                   'date_joined']
         read_only_fields = ['id', 'date_joined']
+    
+    def get_student_profile(self, obj):
+        """
+        Get the student profile data if it exists
+        """
+        if hasattr(obj, 'student_profile') and obj.student_profile:
+            return StudentProfileSerializer(obj.student_profile, context=self.context).data
+        return None
     
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -46,7 +40,19 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         
         return super().update(instance, validated_data)
+
+class StudentProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(source='user', read_only=True)
+    university_details = UniversitySerializer(source='university', read_only=True)
     
+    class Meta:
+        model = StudentProfile
+        fields = ['id', 'university', 'university_details', 'course', 
+                  'created_at', 'updated_at', 'user']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'user', 'university_details']
+        extra_kwargs = {
+            'university': {'write_only': True}
+        }
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
